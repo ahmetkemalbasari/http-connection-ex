da73a064dabc76a337dd96b54c5a6478bb747120
ahmetkemalbasari <akemalbasari@gmail.com>
2024-04-25 14:02:37 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        URL myURL = new URL("https://jsonplaceholder.typicode.com/posts");
+        
+    }
+}
