8bd4c7670335ea98c1c8778301cb146d8fbc8993
ahmetkemalbasari <akemalbasari@gmail.com>
2024-04-25 15:20:55 +0300

    add a check for connection's response status

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         URL myURL = new URL("https://jsonplaceholder.typicode.com/posts");
-        
+        HttpURLConnection myConnection = (HttpURLConnection)myURL.openConnection();
+
+        myConnection.setRequestMethod("GET");
+        if(myConnection.getResponseCode() == 200)
+            System.out.println("Connection successfully established.");
+
     }
 }
