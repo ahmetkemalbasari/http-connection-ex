6d0fa3d47ffdbc609ab55a0ede302dd325cfa50c
ahmetkemalbasari <akemalbasari@gmail.com>
2024-04-25 19:17:01 +0300

    add the part for pulling datas to ram and storing them in a list

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class Main {
+
+    public static ArrayList<Post> posts = new ArrayList<>();
     public static void main(String[] args) throws Exception {
         URL myURL = new URL("https://jsonplaceholder.typicode.com/posts");
         HttpURLConnection myConnection = (HttpURLConnection)myURL.openConnection();
@@ -13,5 +15,25 @@ public class Main {
         if(myConnection.getResponseCode() == 200)
             System.out.println("Connection successfully established.");
 
+        BufferedReader logReader = new BufferedReader(new InputStreamReader(myConnection.getInputStream()));
+
+
+        while(logReader.readLine() != null && logReader.readLine() != null){
+            try {
+                int userId = Integer.parseInt(logReader.readLine().substring(14).replace(",",""));
+                int messageId = Integer.parseInt(logReader.readLine().substring(10).replace(",", ""));
+                String title = logReader.readLine().substring(13);
+                String body = logReader.readLine().substring(12);
+                posts.add(new Post(userId, messageId, title, body));
+            }catch (Exception e){
+                break;
+            }
+
+
+        }
+        System.out.println("Total post count: " + posts.size());
+        for(Post a : posts){
+            System.out.println("used id: " + a.userId + "   post id: " + a.id);
+        }
     }
 }
--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -3,4 +3,11 @@ public class Post {
     public int id;
     public String title;
     public String body;
+
+    public Post(int userId, int id, String title, String body){
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
 }
